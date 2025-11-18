6dc0d85ecaf3c10b32f95f7c8c31f6e35c927131
ycd <hnyoucd@gmail.com>
2025-11-18 13:26:37 +0800

    在线升级功能完善

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 
 
 download_url=`curl  -s https://api.github.com/repos/youcd/WorkReportFrontend_vue3/releases/latest|grep browser_download_url|awk -F"\"" '{print $4}'`
-wget $download_url -O backend/dist.txz
+wget $download_url -O dist.txz
 tar xf dist.txz -C backend/web
 rm -rf dist.txz
 
