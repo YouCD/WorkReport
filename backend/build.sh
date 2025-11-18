f899e41da96b686e74a81b9d0c01b0986cfc04ee
ycd <hnyoucd@gmail.com>
2025-11-18 11:27:23 +0800

    pipeline测试

--- backend/build.sh
+++ backend/build.sh
@@ -2,11 +2,12 @@
 
 
 download_url=`curl  -s https://api.github.com/repos/youcd/WorkReportFrontend_vue3/releases/latest|grep browser_download_url|awk -F"\"" '{print $4}'`
+cd backend
 wget $download_url -O web/dist.txz
-cd web
 tar Jxf dist.txz
+rm -rf dist.txz
 cd ..
 
 wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
 tar xf upx-3.96-amd64_linux.tar.xz
-mv upx-3.96-amd64_linux/upx ./
+mv upx-3.96-amd64_linux/upx /bin/upx
